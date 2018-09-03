5657fd2b6cf172a7215774290adc22223408ec62
syh <syh@10.211.55.3>
2018-09-03 20:11:11 +0800

    finish the module 'individual' add the file 'img'

---
+++ src/vCampus/client/InfoView/Info1_1.java
@@ -0,0 +1,109 @@
+package vCampus.client.InfoView;
+
+import javax.swing.*;
+import java.awt.Font;
+
+
+public class Info1_1 extends JPanel {
+	Info1_1(){		
+	super();
+			Font font=new Font("苹方 常规",Font.CENTER_BASELINE,28);
+			this.setLayout(null);
+			JLabel lb1 = new JLabel("一卡通");
+			JTextField tf1 = new JTextField(20);
+			JLabel lb2 = new JLabel("姓名");
+			JTextField tf2 = new JTextField(20);
+			JLabel lb3 = new JLabel("性别");
+			JTextField tf3 = new JTextField(20);
+			JLabel lb4 = new JLabel("身份证号");
+			JTextField tf4 = new JTextField(20);
+			JLabel lb5 = new JLabel("院系");
+			JTextField tf5 = new JTextField(20);
+			JLabel lb6 = new JLabel("专业");
+			JTextField tf6 = new JTextField(20);
+			JLabel lb7 = new JLabel("班级");
+			JTextField tf7 = new JTextField(20);
+			JLabel lb8 = new JLabel("宿舍号");
+			JTextField tf8 = new JTextField(20);
+			JLabel lb9 = new JLabel("手机");
+			JTextField tf9 = new JTextField(20);
+			JLabel lb10 = new JLabel("邮箱");
+			JTextField tf10 = new JTextField(20);
+		       			
+		    this.add(lb1);
+		    lb1.setBounds(456, 196, 101, 47);
+		    lb1.setFont(font);
+		    this.add(tf1);
+		    tf1.setBounds(660, 196, 352, 47);
+		    tf1.setFont(font);
+		    tf1.setEditable(false);
+		    tf1.setBorder(null);
+
+		    this.add(lb2);
+		    lb2.setBounds(1156, 196, 101, 47);
+		    lb2.setFont(font);
+		    this.add(tf2);
+		    tf2.setBounds(1360, 196, 352, 47);
+		    tf2.setFont(font);
+		    tf2.setEditable(false);
+		    tf2.setBorder(null);
+		    
+		    this.add(lb3);
+		    lb3.setBounds(456, 307, 101, 47);
+		    lb3.setFont(font);
+		    this.add(tf3);
+		    tf3.setBounds(660, 307, 352, 47);
+		    tf3.setFont(font);
+		    tf3.setEditable(false);
+		    tf3.setBorder(null);
+		    
+		    this.add(lb4);
+		    lb4.setBounds(1156, 307, 150, 47);
+		    lb4.setFont(font);
+		    this.add(tf4);
+		    tf4.setBounds(1360, 307, 352, 47);
+		    tf4.setFont(font);
+		    tf4.setEditable(false);
+		    tf4.setBorder(null);
+		    
+		    this.add(lb5);
+		    lb5.setBounds(456, 418, 101, 47);
+		    lb5.setFont(font);
+		    this.add(tf5);
+		    tf5.setBounds(660, 418, 352, 47);
+		    tf5.setFont(font);
+		    tf5.setEditable(false);
+		    tf5.setBorder(null);
+		    
+		    this.add(lb6);
+		    lb6.setBounds(1156, 418, 101, 47);
+		    lb6.setFont(font);
+		    this.add(tf6);
+		    tf6.setBounds(1360, 418, 352, 47);
+		    tf6.setFont(font);
+		    tf6.setEditable(false);
+		    tf6.setBorder(null);
+		    
+		    this.add(lb7);
+		    lb7.setBounds(456, 529, 101, 47);
+		    lb7.setFont(font);
+		    this.add(tf7);
+		    tf7.setBounds(660, 529, 352, 47);
+		    tf7.setFont(font);
+		    tf7.setEditable(false);
+		    tf7.setBorder(null);
+		    
+		    this.add(lb8);
+		    lb8.setBounds(1156, 529, 101, 47);
+		    lb8.setFont(font);
+		    this.add(tf8);
+		    tf8.setBounds(1360, 529, 352, 47);
+		    tf8.setFont(font);
+		    tf8.setEditable(false);
+		    tf8.setBorder(null);
+
+	}
+	}
+
+
+
