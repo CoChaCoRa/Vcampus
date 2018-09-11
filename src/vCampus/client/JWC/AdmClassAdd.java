4d62d16dceacd14304a78571e3976c345265bdf7
syh948918760 <948918760@qq.com>
2018-09-11 10:27:20 +0800

    finish the JWC for Adm

--- src/vCampus/client/JWC/AdmClassAdd.java
+++ src/vCampus/client/JWC/AdmClassAdd.java
@@ -16,6 +16,8 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;  
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class AdmClassAdd extends JPanel{
 	
@@ -56,23 +58,22 @@ public class AdmClassAdd extends JPanel{
 		
 	super();
 
-
-	
 	this.setLayout(null);
 	this.setSize(1650,1000);         
     
     Font font=new Font("苹方 常规",Font.CENTER_BASELINE,28);//设置字体格式和大小
    
     this.add(bt1);
-    bt1.setBounds(910-270, 910-80, 160, 80);
+    bt1.setBounds(870, 772, 160, 80);
     bt1.setFont(font);
     // 设置按钮的默认图片
     bt1.setIcon(new ImageIcon("img\\确认.png"));
     bt1.setBorder(null);
+    bt1.setContentAreaFilled(false);
     
     
     this.add(lb1);
-    lb1.setBounds(456-270, 196-80, 101, 47);
+    lb1.setBounds(456-270, 196-80, 201, 47);
     lb1.setFont(font);
     this.add(tf1);
     tf1.setBackground(Color.WHITE);
@@ -82,7 +83,7 @@ public class AdmClassAdd extends JPanel{
     tf1.setBorder(null);
 
     this.add(lb2);
-    lb2.setBounds(1156-270, 196-80, 101, 47);
+    lb2.setBounds(1156-270, 196-80, 201, 47);
     lb2.setFont(font);
     this.add(tf2);
     tf2.setBackground(Color.WHITE);
@@ -93,7 +94,7 @@ public class AdmClassAdd extends JPanel{
     tf2.setBorder(null);
     
     this.add(lb3);
-    lb3.setBounds(456-270, 307-80, 101, 47);
+    lb3.setBounds(456-270, 307-80, 201, 47);
     lb3.setFont(font);
     this.add(tf3);
     tf3.setBackground(Color.WHITE);
@@ -103,7 +104,7 @@ public class AdmClassAdd extends JPanel{
     tf3.setBorder(null);
     
     this.add(lb4);
-    lb4.setBounds(1156-270, 307-80, 150, 47);
+    lb4.setBounds(1156-270, 307-80, 250, 47);
     lb4.setFont(font);
     this.add(tf4);
     tf4.setBackground(Color.WHITE);
@@ -113,7 +114,7 @@ public class AdmClassAdd extends JPanel{
     tf4.setBorder(null);
     
     this.add(lb5);
-    lb5.setBounds(456-270, 418-80, 101, 47);
+    lb5.setBounds(456-270, 418-80, 201, 47);
     lb5.setFont(font);
     this.add(tf5);
     tf5.setBackground(Color.WHITE);
@@ -124,7 +125,7 @@ public class AdmClassAdd extends JPanel{
     
     this.add(lb6);
     tf6.setBackground(Color.WHITE);
-    lb6.setBounds(1156-270, 418-80, 101, 47);
+    lb6.setBounds(1156-270, 418-80, 201, 47);
     lb6.setFont(font);
     this.add(tf6);
     tf6.setBounds(1360-270, 418-80, 352, 47);
@@ -136,7 +137,7 @@ public class AdmClassAdd extends JPanel{
 
     this.add(lb7);
     tf7.setBackground(Color.WHITE);
-    lb7.setBounds(456-270, 529-80, 101, 47);
+    lb7.setBounds(456-270, 529-80, 201, 47);
     lb7.setFont(font);
     this.add(tf7);
     tf7.setBounds(660-270, 529-80, 352, 47);
@@ -145,7 +146,7 @@ public class AdmClassAdd extends JPanel{
     tf7.setBorder(null);
     
     this.add(lb8);
-    lb8.setBounds(1156-270, 529-80, 101, 47);
+    lb8.setBounds(1156-270, 529-80, 201, 47);
     lb8.setFont(font);
     this.add(tf8);
     tf8.setBackground(Color.WHITE);
@@ -155,7 +156,7 @@ public class AdmClassAdd extends JPanel{
     tf8.setBorder(null);
    
     this.add(lb9);
-    lb9.setBounds(456-270, 640-80, 101, 47);
+    lb9.setBounds(456-270, 640-80, 201, 47);
     lb9.setFont(font);
     this.add(tf9);
     tf9.setBackground(Color.WHITE);
@@ -165,7 +166,7 @@ public class AdmClassAdd extends JPanel{
     tf9.setBorder(null);
     
     this.add(lb10);
-    lb10.setBounds(1156-270, 640-80, 101, 47);
+    lb10.setBounds(1156-270, 640-80, 201, 47);
     lb10.setFont(font);
     this.add(tf10);
     tf10.setBackground(Color.WHITE);
@@ -176,17 +177,17 @@ public class AdmClassAdd extends JPanel{
   
 
     this.add(lb11);
-    lb11.setBounds(456-270, 640-80+111, 101, 47);
+    lb11.setBounds(456-270, 640-80+111, 201, 47);
     lb11.setFont(font);
     this.add(tf11);
     tf11.setBackground(Color.WHITE);
-    tf11.setBounds(660-270, 640-80, 352, 47);
+    tf11.setBounds(660-270, 640-80+111, 352, 47);
     tf11.setFont(font);
     tf11.setEditable(false);
     tf11.setBorder(null);
     
     this.add(lb12);
-    lb12.setBounds(1156-270, 640-80+111, 101, 47);
+    lb12.setBounds(1156-270, 640-80+111, 201, 47);
     lb12.setFont(font);
     this.add(tf12);
     tf12.setBackground(Color.WHITE);
@@ -197,7 +198,7 @@ public class AdmClassAdd extends JPanel{
   
 
     this.add(lb13);
-    lb13.setBounds(456-270, 640-80+111*2, 101, 47);
+    lb13.setBounds(456-270, 640-80+111*2, 201, 47);
     lb13.setFont(font);
     this.add(tf13);
     tf13.setBackground(Color.WHITE);
@@ -206,11 +207,38 @@ public class AdmClassAdd extends JPanel{
     tf13.setEditable(false);
     tf13.setBorder(null);
     
+    bt1.addMouseListener(new MouseListener() {
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			bt1.setIcon(new ImageIcon("img\\确认点击.png"));
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			bt1.setIcon(new ImageIcon("img\\确认.png"));
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			 
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub				
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub				
+		}
+    });
+
+
     this.setLayout(null);
 	this.setSize(1650,1000);         
-    
+	
 
-	
-	
 	}
+	
 }
